d78fd9a2c1c9dbead3d780ce371168a374ae0405
Mohit <mohit.bharti566@gmail.com>
2021-04-14 20:45:36 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={window.location.pathname || ''}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
